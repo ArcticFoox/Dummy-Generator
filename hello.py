814cf68fabc7fa4636c3f1d5ca7d0a23ee42278a
신종신 <jongsin16@naver.com>
2024-08-29 15:50:44 +0900

    feat: 초기 10000 데이터 생성 완료

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print('hello world')
+from faker import Faker
+import pandas as pd
+
+fake = Faker()
+kr_fake = Faker('ko_KR')
+Faker.seed()
+
+data_cnt = 10000
+
+name = [kr_fake.name() for i in range(data_cnt)]
+username = [fake.user_name() for i in range(data_cnt)]
+
+df = pd.DataFrame()
+df['user_id'] = username
+df['user_name'] = name
+
+df.to_csv("dummy.csv", encoding='utf-8')
